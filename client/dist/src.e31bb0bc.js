48a38b2b2c0b0f7e2134006e07c616fa8d73e2e7
Dheeraj Mohan <dheerajmohan98@gmail.com>
2020-06-26 05:22:52 +0530

    UI bug fix

--- client/dist/src.e31bb0bc.js
+++ client/dist/src.e31bb0bc.js
@@ -49675,7 +49675,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "34127" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "46707" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
